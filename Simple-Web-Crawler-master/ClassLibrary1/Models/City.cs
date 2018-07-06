b56ebea17c2d48396f1c55e9fa2e830f31f8a4a4
zuoluo11 <zuoluo11@qq.com>
2018-07-06 14:26:25 +0800

    s

---
+++ Simple-Web-Crawler-master/ClassLibrary1/Models/City.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1.Models
+{
+    public class City
+    {
+        public string CityName { get; set; }
+
+        public Uri Uri { get; set; }
+    }
+
+  
+}
---
+++ Simple-Web-Crawler-master/WebApplication1/Handler1.ashx.cs
@@ -0,0 +1,32 @@
+﻿using ClassLibrary1.Models;
+using ClassLibrary1.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace WebApplication1
+{
+    /// <summary>
+    /// Handler1 的摘要说明
+    /// </summary>
+    public class Handler1 : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain";
+            var result = ProductService.GetAllProduct();
+            context.Response.Write(result);
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
